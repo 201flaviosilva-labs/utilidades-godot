77c35b23c0ddb29b40c3ba75feca70d9dcb34307
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-12-03 21:49:54 +0000

    add: delete_children

--- Utils/addons/201flaviosilva-utils/Arrays.gd
+++ Utils/addons/201flaviosilva-utils/Arrays.gd
@@ -1,5 +1,10 @@
 extends Node
 
-func choice(arr):
+static func choice(arr):
 	randomize()
 	return arr[randi() % arr.size()]
+
+static func delete_children(node: Node):
+	for child_node in node.get_children():
+		node.remove_child(child_node)
+		child_node.queue_free()
